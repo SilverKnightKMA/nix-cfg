c8d4c1a39444818474f2099a755e6d75a08960a3
Vũ Thanh Tùng <at160758@actvn.edu.vn>
2025-05-30 22:38:16 +0700

    grub device

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -15,6 +15,7 @@
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.efiSupport = true;
 
   networking.hostName = "t14"; # Define your hostname.
