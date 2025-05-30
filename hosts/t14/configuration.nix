6cc78efa199a865ed48e18ae8069de8b4eb725fb
Vũ Thanh Tùng <at160758@actvn.edu.vn>
2025-05-30 23:07:36 +0700

    efi

--- hosts/t14/configuration.nix
+++ hosts/t14/configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   #boot.loader.grub.enable = true;
   #boot.loader.grub.device = "/dev/sda";
-  #boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiSupport = true;
 
   networking.hostName = "t14"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
